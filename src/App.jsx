86f097bb42c82fb69c1e08f23a81c43a93ff435d
Elamathi26 <elamathi2611@gmail.com>
2024-04-15 11:48:19 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+  
+import './App.css'
+
+function App() {
+ return (
+    <>
+<Greet/>
+    </>
+  )
+}
+
+function Greet(){
+return (
+  <div>
+    <h1>
+      Hello world
+    </h1>
+  </div>
+)
+
+}
+
+export default App
